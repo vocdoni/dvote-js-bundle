94e0c7e716af07a3d99f06c2defe34860f4801fc
Jør∂¡ <itsbrickpop@gmail.com>
2022-01-20 16:31:52 +0100

    Exposing indirect dependencies on main.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,19 @@
 const voting = require("@vocdoni/voting")
 const census = require("@vocdoni/census")
+const dataModels = require("@vocdoni/data-models")
+const signing = require("@vocdoni/signing")
+const encryption = require("@vocdoni/encryption")
+const common = require("@vocdoni/common")
+const contractWrappers = require("@vocdoni/contract-wrappers")
 
 window.getVocdoni = () => {
    return {
       ...voting,
-      ...census
+      ...census,
+      ...dataModels,
+      ...signing,
+      ...encryption,
+      ...common,
+      ...contractWrappers
    }
 }
